5a023ff0e91afafa41f9c07078ce3fa0e5ff2ed3
Fermin <fmfs@posteo.net>
2023-06-25 20:10:11 +0200

    Add lemmington README

---
+++ README.md
@@ -0,0 +1,23 @@
+# Lemmington
+
+This is a package for RPC integrations with from GNU Emacs to  [Lem](https://github.com/lem-project/lem).
+
+## Installation
+
+For now, add the files to GNU Emacs load-path
+
+```elisp
+
+
+(add-to-list 'load-path "/path/to/lemmington/")
+
+
+```
+
+
+## Usage
+
+To start the server call `lemmington-start-server`, the default port is 55486, every other function is meant to be called from Lem.
+
+
+
---
+++ lemmington.el
@@ -0,0 +1,104 @@
+;;; lemmington.el --- RPC Emacs server and REPL             -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Fermin Munoz
+
+;; Author: Fermin Munoz
+;; Maintainer: Fermin Munoz <fmfs@posteo.net>
+;; Created: 25 June 2023
+;; Version: 0.0.1
+;; Keywords: tools,languages
+;; URL: https://codeberg.org/sasanidas/lemmigton
+;; Package-Requires: ((emacs "27.1")(porthole "0.3.0"))
+;; License: GPL-3.0-or-later
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
+;;;; The requires
+
+(require 'cl-lib)
+(require 'porthole)
+
+;; (lem-symbol-location "goto-char")
+;; (lem-symbol-location "pi")
+;; (lem-symbol-location "asdasd")
+
+(cl-defun lemmington-start-server (&key
+				   (name "lemmington-server")
+				   (port 55486)
+				   (username "lem")
+				   (password "lem"))
+  "Start the porthole server.
+Optionally, you can set as a key argument:
+NAME : name of the server.
+PORT: port of the server.
+USERNAME: username for the basic-auth.
+PASSWORD: password for the basic-auth."
+  (porthole-start-server-advanced
+   name
+   :port port
+   :username username
+   :password password
+   :publish-port nil
+   :publish-username t
+   :publish-password t))
+
+
+(cl-defun lem-get-completion (prefix)
+  "Return a list of symbols with PREFIX as prefix."
+  (elisp-refs--filter-obarray (lambda (i)
+				(string-prefix-p prefix (format "%s" i) t))))
+
+(cl-defun lem-symbol-location (symbol)
+  "Return a a list of (file absolute-position) of SYMBOL."
+  (let ((symbol-intern (intern-soft symbol))
+	symbol-info)
+    (cond
+     ((functionp symbol-intern)
+      (setf symbol-info
+	    (find-definition-noselect symbol-intern nil)))
+     ((boundp symbol-intern)
+      (setf symbol-info
+	    (find-definition-noselect symbol-intern 'defvar)))
+     (t
+      (cl-return-from lem-symbol-location
+	nil)))
+    (list (buffer-file-name (car symbol-info))
+	  (cdr symbol-info))))
+
+(cl-defun lem-symbol-documentation (symbol)
+  "Return SYMBOL documentation."
+  (let ((symbol-intern (intern-soft symbol)))
+    (cond
+     ((functionp symbol-intern)
+      (documentation symbol-intern))
+     ((boundp symbol-intern)
+      (documentation-property symbol-intern 'variable-documentation))
+     (t
+      (cl-return-from lem-symbol-documentation
+	nil)))))
+
+(defvar *lemmigton-export-functions*
+  '(lem-get-completion
+    lem-symbol-location
+    lem-symbol-documentation))
+
+(cl-defun lemmington-export-functions (&key
+				       (server "lemmington-server")
+				       (functions *lem-emacs-export-functions*))
+  "Export FUNCTIONS to SERVER."
+  (mapcar (lambda (fname)
+	    (porthole-expose-function server fname))
+	  functions))
